e86f0e08ffc182f89b4f2ace59fa033e988d0799
Alan Bacon <alan.bacon@bbc.co.uk>
2019-08-22 23:09:36 +0100

    initial commit

---
+++ tests/StringQuartet_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestStringQuartet(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def test_should_pass(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+	unittest.main()
